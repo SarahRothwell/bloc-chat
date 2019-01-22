b74fbf37c3d8d6661017d7bb83683ee2dc3e1f94
Sarah.Rothwell <sarahhrothwell@gmail.com>
2019-01-22 17:02:05 -0500

    checkpoint submission

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js';
 
 var config = {
     apiKey: "AIzaSyD4oMBI2mzH-vSZKo6hLf4wcRrhTBu9brE",
@@ -15,23 +15,21 @@ var config = {
 
 
 class App extends Component {
+  constructor() {
+    super()
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Bloc Chat</h1>
         </header>
+        <main>
+          <div>
+          <RoomList firebase={firebase}/>
+          </div>
+        </main>
       </div>
     );
   }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 
 class RoomList extends Component {
-  constructor(props){
+  constructor (props) {
     super(props);
 
-    this.state = { rooms: []};
+    this.state ={
+      rooms: []
+    };
 
+    this.roomsRef = this.props.firebase.database().ref('rooms');
   }
 
-  this.roomsRef = this.props.firebase.database().ref('rooms');
+componentDidMount(){
+  this.roomsRef.on('child_added', snapshot => {
+    const room = snapshot.val();
+    room.key = snapshot.key;
+    this.setState({ rooms: this.state.rooms.concat( room ) })
+  });
+}
 
-  render(){
-    return(
 
+  render () {
+    return (
+      <section className = "room-list-wrapper">
+        <ul className="chat-rooms">
+          {this.state.rooms.map((room, index) =>
+            <li className = "room-names" key={index}>{room.name}</li>)}
+        </ul>
+      </section>
     );
   }
 }
 
-export default App;
+export default RoomList;
