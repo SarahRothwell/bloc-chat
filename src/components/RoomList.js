d6405e4494e95562e3d38461a533402c317ab5f0
Sarah.Rothwell <sarahhrothwell@gmail.com>
2019-01-23 14:17:57 -0500

    add room error

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -9,7 +9,29 @@ class RoomList extends Component {
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.createRoom = this.createRoom.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+handleChange(e){
+  this.setState({name: e.target.value});
+}
+
+/*form submission logic to create new room in firebase*/
+createRoom(e){
+  e.preventDefault();
+  let newRoomName = e.target.value;
+  console.log(newRoomName);
+  this.roomsRef.push({
+    name: newRoomName
+  });
+}
+
+/*Cancel form logic*/
+handleCancelForm(){
+
+}
+
 
 componentDidMount(){
   this.roomsRef.on('child_added', snapshot => {
@@ -27,6 +49,17 @@ componentDidMount(){
           {this.state.rooms.map((room, index) =>
             <li className = "room-names" key={index}>{room.name}</li>)}
         </ul>
+        <form className = "create-new-room" onSubmit={this.createRoom}>
+          <label>Enter name of new room:</label><br/>
+          <input
+            id = "submitNewRoom"
+            type="text"
+            name="name"
+            placeholder="Name of Room..."
+            value={this.state.name}
+            onChange={e => this.handleChange(e)}/><br/>
+          <button id='submitForm'>Submit</button><br/>
+        </form>
       </section>
     );
   }
